d3fc54c090062967470f1b84150f2bc93b88946f
Tim Eggers <tim.luca.eggers@mni.thm.de>
2021-11-17 10:41:54 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { MapComponent } from './map/map.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    MapComponent,
+    UserListComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule
