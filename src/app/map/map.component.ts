3b24e6e5eb1ae7f4c4db33a54f360e6bb9296c6a
Marco Diaz Garcia <marco.diaz.garcia@mni.thm.de>
2021-11-24 16:26:53 +0100

    markers reloading is reworked

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,8 +1,9 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {MapService} from "../services/map.service";
 import {User} from "../models/user";
-import {interval, map, startWith, Subscription, switchMap} from "rxjs";
+import {interval, Subscription, switchMap} from "rxjs";
 import {GoogleMap, MapInfoWindow, MapMarker} from "@angular/google-maps";
+import Animation = google.maps.Animation;
 
 @Component({
   selector: 'app-map',
@@ -17,12 +18,12 @@ export class MapComponent implements OnInit {
   constructor(private mapService: MapService) {
   }
 
-  users: User[] = [];
-  timeInterval!: Subscription;
+  public users: User[] = [];
+  public timeInterval!: Subscription;
 
-  markers: any[] = [];
-  infoContent = "";
-  center!: google.maps.LatLngLiteral;
+  public markers: Map<string, google.maps.Marker> = new Map();
+  public infoContent = "";
+  public center!: google.maps.LatLngLiteral;
 
   ngOnInit(): void {
     // this.createMarkers().then()
@@ -43,10 +44,16 @@ export class MapComponent implements OnInit {
       switchMap(() => this.mapService.getUserPositions()),
     ).subscribe({
       next: (res: any) => {
-        // for (const re of res) {
-        //   this.addMarker(re);
-        // }
         this.users = res;
+        for (let user of res) {
+          if(this.markers.has(user.id)) {
+            if (this.isNewMarker(user)) {
+              this.addNewMarker(user);
+            }
+          } else {
+            this.addNewMarker(user);
+          }
+        }
       },
       error: (e) => console.error(e),
       complete: () => console.info('complete')
@@ -55,10 +62,34 @@ export class MapComponent implements OnInit {
 
   }
 
+  private isNewMarker(user: User): boolean {
+    let pos = this.markers.get(user.id)!.getPosition();
+    return user.latestPositions![0].lat != pos?.lat() || user.latestPositions![0].lng != pos?.lng();
+  }
+
+  private addNewMarker(user: User) {
+    if (user.latestPositions != undefined) {
+      let oldMarker: google.maps.Marker | undefined;
+      let newPos = {lat: user.latestPositions[0].lat, lng: user.latestPositions[0].lng}
+      oldMarker = this.markers.get(user.id);
+      this.markers.set(user.id, new google.maps.Marker({
+        position: newPos,
+        title: user.username,
+        label: {
+          color: "black",
+          text: user.username
+        }
+      }));
+      if (oldMarker) oldMarker.setMap(null);
+      this.markers.get(user.id)!.setMap(this.map.googleMap!);
+      //TODO: add click Event Listener
+    }
+  }
+
 
   mapOptions: google.maps.MapOptions = {
     center: new google.maps.LatLng(50.58727, 8.67554),
-    zoom: 2,
+    zoom: 9,
     streetViewControl: false,
     fullscreenControl: false,
     // maxZoom: 15,
@@ -70,44 +101,9 @@ export class MapComponent implements OnInit {
     opacity: 0.8,
   }
 
+  //TODO: routes
   polylineOptions: google.maps.PolylineOptions = {
     path: []
   }
-
-  addMarker(re: User) {
-    if(re.latestPositions != undefined) {
-      this.markers.push({
-        position: {
-          lat: re.latestPositions[0].lat + 5,
-          lng: re.latestPositions[0].lng,
-        },
-        label: {
-          color: "red",
-          text: re.username,
-        },
-        title: 'Marker title ' + (this.markers.length + 1),
-        info: 'Marker info ' + (this.markers.length + 1),
-        options: {
-          // animation: google.maps.Animation.BOUNCE,
-        },
-      })
-    }
-  }
-
-  openInfo(marker: MapMarker, content: any) {
-    this.infoContent = content
-    this.info.open(marker)
-  }
-
-  logCenter() {
-    console.log(JSON.stringify(this.map.getCenter()))
-  }
-
-
-  // m: google.maps.Marker = new google.maps.Marker({
-  //   position: new google.maps.LatLng(50, 8),
-  //   title: "m"
-  // })
-
 
 }
