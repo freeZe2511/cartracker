b277918daa9b10b64b44fe3e7f75f20f17d8080a
Tim Eggers <tim.luca.eggers@mni.thm.de>
2022-01-14 00:46:32 +0100

    imprint component + in header and login

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import {UserListComponent} from "./components/user-list/user-list.component";
 import {ZoneListComponent} from "./components/zone-list/zone-list.component";
 import {AuthGuard} from "./shared/guards/auth.guard";
 import {LoginComponent} from "./components/login/login.component";
+import {ImprintComponent} from "./components/imprint/imprint.component";
 
 const routes: Routes = [
   {path: '', redirectTo: '/sign-in', pathMatch: 'full'},
   {path: 'sign-in', component: LoginComponent},
   {path: 'home', redirectTo: '/map'},
+  {path: 'imprint', component: ImprintComponent},
   {path: 'map', component: MapComponent, canActivate: [AuthGuard]},
   {path: 'users', component: UserListComponent, canActivate: [AuthGuard]},
   {path: 'zones', component: ZoneListComponent, canActivate: [AuthGuard]},
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -34,6 +34,7 @@ import {ZoneListComponent} from './components/zone-list/zone-list.component';
 import {CommonModule} from "@angular/common";
 import {SimpleNotificationsModule} from "angular2-notifications";
 import { EditZoneModalComponent } from './components/edit-zone-modal/edit-zone-modal.component';
+import { ImprintComponent } from './components/imprint/imprint.component';
 
 @NgModule({
   declarations: [
@@ -50,7 +51,8 @@ import { EditZoneModalComponent } from './components/edit-zone-modal/edit-zone-m
     UserInfoWindowComponent,
     AddZoneModalComponent,
     ZoneListComponent,
-    EditZoneModalComponent
+    EditZoneModalComponent,
+    ImprintComponent
   ],
   imports: [
     BrowserModule,
