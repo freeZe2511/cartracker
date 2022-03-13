364763196a38a2dcf1fea174bb1d3727bb27a89c
Tim Eggers <tim.luca.eggers@mni.thm.de>
2022-03-13 15:48:21 +0100

    new

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,15 +12,13 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {FlexLayoutModule} from "@angular/flex-layout";
 import {RouterModule} from "@angular/router";
 import {HttpClientModule} from "@angular/common/http";
-import { ToolbarComponent } from './components/toolbar/toolbar.component';
-import { UserListComponent } from './components/user-list/user-list.component';
-import { ZoneListComponent } from './components/zone-list/zone-list.component';
-import { StatisticsComponent } from './components/statistics/statistics.component';
-import { HelpComponent } from './components/help/help.component';
-import {MatSlideToggleModule} from "@angular/material/slide-toggle";
-import { DialogComponent } from './components/dialog/dialog.component';
+import {ToolbarComponent} from './components/toolbar/toolbar.component';
+import {UserListComponent} from './components/user-list/user-list.component';
+import {ZoneListComponent} from './components/zone-list/zone-list.component';
+import {StatisticsComponent} from './components/statistics/statistics.component';
+import {HelpComponent} from './components/help/help.component';
+import {DialogComponent} from './components/dialog/dialog.component';
 import {SimpleNotificationsModule} from "angular2-notifications";
-import {MatSliderModule} from "@angular/material/slider";
 
 @NgModule({
   declarations: [
@@ -45,7 +43,6 @@ import {MatSliderModule} from "@angular/material/slider";
     ReactiveFormsModule,
     FlexLayoutModule,
     HttpClientModule,
-    MatSlideToggleModule,
     SimpleNotificationsModule.forRoot(),
   ],
   providers: [],
